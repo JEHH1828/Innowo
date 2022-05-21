161df7342d838f6ab61aab86956ca53bba3955b3
Emilio HH <jehh1828@gmail.com>
2022-05-20 22:20:54 -0500

    libros y demas agregado y quitado

--- innowo/assets/js/main.js
+++ innowo/assets/js/main.js
@@ -857,3 +857,18 @@
     
     
 })(jQuery);	
+    // popovers initialization - on hover
+    $('[data-toggle="popover-hover"]').popover({
+        html: true,
+        trigger: 'hover',
+        placement: 'bottom',
+        content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+      });
+  
+      // popovers initialization - on click
+      $('[data-toggle="popover-click"]').popover({
+        html: true,
+        trigger: 'click',
+        placement: 'bottom',
+        content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+      });
